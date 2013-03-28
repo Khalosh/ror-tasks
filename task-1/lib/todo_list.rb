47fe5140ab8f37eaecb2c82339873531d58c875e
Krzysztof Chomicki <khalosh@gmail.com>
2013-03-28 06:14:49 +0100

    Task-1 completed

--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -1,6 +1,118 @@
 class TodoList
 
+  class Item
+  attr_accessor :description, :completed
+
+    def initialize(description=nil)
+      @description = description
+      @completed = false
+    end
+    
+    def to_s
+      @description.to_s
+    end
+
+    def format
+      if @completed
+        completion = "x"
+      else
+        completion = " "
+      end
+      "- [#{completion}] #{@description}"
+    end
+
+  end
+
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
+    raise IllegalArgument if items.nil?
+    @items = []
+      items.each do |item|
+        @items.push Item.new(item)
+      end
   end
+
+  def size
+    @items.size
+  end
+
+  def item(index)
+    @items[index]
+  end
+
+  def completed?(index)
+    @items[index].completed
+  end
+
+  def complete(index)
+    @items[index].completed = true
+  end
+
+  def completed 
+    @items.select { |item| item.completed }
+  end
+
+  def uncomplete(index)
+    @items[index].completed = false
+  end
+
+  def uncompleted
+    @items.select { |item| !item.completed }
+  end
+
+  def remove(index)
+    @items.delete_at(index)
+  end
+
+  def remove_completed
+    @items.reject! { |item| item.completed }
+  end
+
+  def reverse(one, two)
+    first = @items[one]
+    second = @items[two]
+    @items[one] = second
+    @items[two] = first
+  end
+
+  def reverse_all
+    @items.reverse!
+  end
+
+  def sort
+    @items.sort! { |a,b| a.description <=> b.description }
+  end
+
+  def to_s
+    (@items.collect { |item| item.format }).join("\n")
+  end
+
+  def toggle(index)
+    if @items[index].completed
+      @items[index].completed = false
+    else
+      @items[index].completed = true
+    end
+  end
+
+  def change_desc(index, desc)
+    @items[index].description = desc
+  end
+
+  def empty?
+    @items.empty?
+  end
+
+  def << (item)
+    @items.push(Item.new(item))
+  end
+
+  def last
+    @items.last
+  end
+
+  def first
+    @items.first
+  end
+    
 end
--- task-1/spec/todo_list.rb
+++ task-1/spec/todo_list.rb
@@ -7,6 +7,8 @@ describe TodoList do
   subject(:list)            { TodoList.new(items) }
   let(:items)               { [] }
   let(:item_description)    { "Buy toilet paper" }
+  let(:second_item_description) { "Buy oranges" }
+  let(:third_item_description)  { "Cook the potatoes" }
 
   it { should be_empty }
 
@@ -54,5 +56,82 @@ describe TodoList do
       list.complete(0)
       list.completed?(0).should be_true
     end
+  end
+
+  context "with many items" do
+    let(:items)               { [item_description, 
+second_item_description, third_item_description] }
+   
+    it "should return completed items" do
+     list.complete(0)
+     list.completed.to_s.should == "["+list.item(0).to_s+"]"
+    end
+
+    it "should return uncompleted items" do
+      list.complete(0)
+      list.complete(2)
+      list.uncompleted.to_s.should == "["+list.item(1).to_s+"]"
+    end
+
+    it "should remove an individual item" do
+      list.remove(0)
+      list.item(0).to_s.should == second_item_description.to_s
+    end
+    
+    it "should remove completed items" do
+      list.complete(0)
+      list.complete(1)
+      list.remove_completed.to_s.should == "["+third_item_description.to_s+"]"
+    end
+
+    it "should reverse order of two items" do
+      first = list.item(0)
+      second = list.item(1)
+      list.reverse(0,1)
+      first.should == list.item(1)
+      second.should == list.item(0)
+    end
+
+    it "should reverse all items" do
+      items = []
+      for i in (0...list.size) do
+        items.push(list.item(i))
+      end
+      list.reverse_all
+      for i in (0...list.size) do
+        items[items.size - i - 1].to_s.should == list.item(i).to_s
+      end
+    end
+
+    it "should toggle state of an item" do
+      list.toggle(0)
+      list.completed?(0).should be_true
+      list.toggle(0)
+      list.completed?(0).should be_false
+    end
+
+    it "should make item uncompleted" do
+      list.complete(0)
+      list.uncomplete(0)
+      list.completed?(0).should be_false
+    end
+
+    it "should change the description of an item" do
+      list.change_desc(0, "New description")
+      list.item(0).description.should == "New description"
+    end
+
+    it "should sort items by name" do
+      list.sort
+      for i in (0...(list.size - 1)) do
+        list.item(i).to_s.should <= list.item(i + 1).to_s
+      end
+    end
+
+    it "should convert the list to text with the following format:\n- [ ] Uncompleted item\n- [x] Completed item" do
+      list.complete(1)
+      list.to_s.should == "- [ ] #{item_description}\n- [x] #{second_item_description}\n- [ ] #{third_item_description}"
+    end
+
   end
 end
