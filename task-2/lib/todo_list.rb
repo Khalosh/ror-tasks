e9807026200de0eb240a00535752d204b3814c29
Krzysztof Chomicki <khalosh@gmail.com>
2013-04-04 00:12:36 +0200

    task-2 completed

--- task-2/lib/todo_list.rb
+++ task-2/lib/todo_list.rb
@@ -1,6 +1,66 @@
 class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
-  def initialize(items=[])
+
+  def initialize(parameters=[])
+    @network = parameters[:social_network]
+    @parameters = parameters
+    raise IllegalArgument if parameters[:db].nil?
+    
+   end
+
+  
+  def empty?
+    @parameters[:db].items_count == 0
   end
+  
+  def size
+    @parameters[:db].items_count
+  end
+  
+  def << (item)
+    return nil if item == nil
+    if item[:title] == "" || item[:title].length <6
+      false
+    else
+      @parameters[:db].add_todo_item(item)
+      if @network
+        item.title = item.title[0 .. 254] if item.title.length >= 255
+        @network.spam(item)
+      end
+    end
+  end
+  
+  def first
+    @parameters[:db].get_todo_item(0)
+  end
+  
+  def last
+    size = @parameters[:db].items_count
+    if size == 0
+      @parameters[:db].get_todo_item(size)
+    else
+      number = size-1
+      @parameters[:db].get_todo_item(number)
+    end
+  end
+
+  def toggle_state (index)
+    item = @parameters[:db].get_todo_item(index)
+
+    if item == nil
+      raise IllegalArgument
+    else
+      if @parameters[:db].todo_item_completed?(index) == false
+        @parameters[:db].complete_todo_item(index,true)
+        if @network
+          item.title = item.title[0 .. 254] if item.title.length >= 255
+          @network.spam(item)
+        end
+      else
+        @parameters[:db].complete_todo_item(index,false)
+      end
+    end
+  end
+
 end
--- task-2/spec/todo_list.rb
+++ task-2/spec/todo_list.rb
@@ -38,12 +38,14 @@ describe TodoList do
   end
 
   it "should persist the state of the item" do
+    mock(database).get_todo_item(0) {item}
     mock(database).todo_item_completed?(0) { false }
     mock(database).complete_todo_item(0,true) { true }
     mock(database).todo_item_completed?(0) { true }
     mock(database).complete_todo_item(0,false) { true }
-
+     
     list.toggle_state(0)
+    mock(database).get_todo_item(0) {item} 
     list.toggle_state(0)
   end
 
@@ -72,6 +74,102 @@ describe TodoList do
       dont_allow(database).add_todo_item(item)
 
       list << item
+    end
+  end
+
+  context "with title too short" do
+    let(:title) { "torsk" }
+
+    it "should not accept item" do
+      dont_allow(database).add_todo_item(item)
+      list << item
+    end
+  end
+
+  context "with missing description" do
+    let(:description) {nil}
+    
+    it "should accept an item" do
+      mock(database).add_todo_item(item) { true }
+      list << item 
+    end
+  end
+
+  context "with empty DB" do
+    let(:item) {nil}
+  
+    it "should return nil for the first and last item" do
+      stub(database).items_count {0}
+
+      mock(database).get_todo_item(0) {nil}
+      list.first.should == nil
+
+      mock(database).get_todo_item(0) {nil}
+      list.last.should == nil
+    end
+ 
+    it "should raise and exception when changing the item state if the item is nil" do
+      mock(database).get_todo_item(0) {nil}
+      expect {list.toggle_state(0) }.to raise_error(IllegalArgument)
+    end
+   
+    it "should not accept a nil item" do
+      dont_allow(database).add_todo_item(item)
+      list << item
+    end
+  end
+
+  context "with social network" do
+      subject(:list)            { TodoList.new(db: database, social_network: network) }
+      let(:network) {stub!}
+    it "should notify if an item is added" do
+      mock(database).add_todo_item(item) {true}
+      mock(network).spam(item) {true}
+      list << item
+    end
+
+    it "should notify if an item is completed" do
+      mock(database).get_todo_item(0) {item}
+      mock(database).todo_item_completed?(0) { false }
+      mock(database).complete_todo_item(0, true)
+      mock(network).spam(item) {true}
+      list.toggle_state(0)
+    end
+
+    context "with missing title" do
+      let(:title) {nil}
+      it "should not notify if the title is missing" do
+        dont_allow(network).spam(item) {true}  
+      end
+    end
+ 
+    context "with missing body" do
+      let(:body) {nil}
+      it "should notify if the body is missing" do
+        mock(database).add_todo_item(item) {true}
+        mock(network).spam(item) {true}
+        list << item
+      end
+    end
+
+    context "with title longer than 255 chars" do
+      let(:title)   { "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." }
+
+      it "should cut the title when notifying the social network" do
+        mock(database).add_todo_item(item) { true } 
+        mock(network).spam(item) { true }
+        
+        list << item	
+        
+        mock(database).get_todo_item(0) {item}
+        mock(database).todo_item_completed?(0) { false }
+        mock(database).complete_todo_item(0, true)
+        mock(network).spam(item) {true}
+        list.toggle_state(0)
+
+	item.title.length.should <= 255
+
+      end
     end
   end
 end
